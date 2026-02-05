eaeccf23ff30fb5377fcaee1c46723774e0c7def
Victor Hdez <atlante_infinito@outlook.com>
2026-02-04 21:59:34 -0600

    ADD :: Delete compiled files

--- pass_generator.pas
+++ pass_generator.pas
@@ -67,9 +67,6 @@ type
         public
     end; { -- END: FORM CLASS -- }
 
-
-
-
 var // LOCAL VARIABLES
     main_form: Tmain_form;
     PWData : TPWData = ( pwmin_size:9; pwmax_size :9; LWCASE :False; UPPCASE :False; numbers :True; special :False; );
