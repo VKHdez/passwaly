22d3baba19bc68113ffef8c385cf80a631d09186
Victor Hdez <atlante_infinito@outlook.com>
2026-02-04 21:55:08 -0600

    FEAT :: Finishing UI events

---
+++ string_operator.pas
@@ -0,0 +1,20 @@
+unit utilities;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+      Classes, SysUtils;
+
+type
+    function getRandomIntBetweenValues(minValue : Integer, maxValue : Integer) : Integer;
+
+implementation
+
+function getRandomIntBetweenValues(const minValue : Integer, const maxValue : Integer) : Integer;
+begin
+
+
+end.
+
